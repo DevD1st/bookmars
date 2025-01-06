893198246c3c8e8211ec3bf2e5b347080d9b295d
olanrewajuyusuf379@gmail.com <olanrewajuyusuf379@gmail.com>
2025-01-06 17:54:37 +0100

    Add signin functionality and enhance user service tests with bcrypt and JWT verification

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -4,17 +4,19 @@ import { CreateUserRequestDto } from "./dtos/create-user-request.dto";
 import { User } from "./user.entity";
 import { userService } from "./user.service";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { SigninRequestDto } from "./dtos/signin-request.dto";
 
 describe("UserService", () => {
+  const id = 1;
+  const email = "test@test.com";
+  const password = "password";
+  const profilePicture = "https://example.com/example.png";
+  const firstName = "firstName";
+  const lastName = "lastName";
+
   describe("signup", () => {
     describe("Given valid request body", () => {
-      const id = 1;
-      const email = "test@test.com";
-      const password = "password";
-      const profilePicture = "https://example.com/example.png";
-      const firstName = "firstName";
-      const lastName = "lastName";
-
       it("should create a new user", async () => {
         const dto = new CreateUserRequestDto();
         dto.email = email;
@@ -45,7 +47,7 @@ describe("UserService", () => {
           save: jest.fn().mockResolvedValueOnce(savedUser),
         });
 
-        jest.spyOn(jwt, "verify").mockImplementation(
+        jest.spyOn(jwt, "verify").mockImplementationOnce(
           () =>
             ({
               id,
@@ -60,6 +62,58 @@ describe("UserService", () => {
         expect(statusCode).toBe(201);
         expect(message).toContain("success");
         expect(data).not.toBeNull();
+      });
+    });
+  });
+
+  describe("signin", () => {
+    describe("given valid request body", () => {
+      it("should sign in the user", async () => {
+        const dto = new SigninRequestDto();
+        dto.email = email;
+        dto.password = password;
+
+        const profile = new Profile();
+        profile.dob = new Date();
+        profile.id = id;
+        profile.profilePicture = profilePicture;
+
+        const savedUser = {
+          id,
+          firstName,
+          lastName,
+          email,
+          password,
+          isActive: true,
+          profile,
+          auth: [],
+        } as any;
+
+        jest.spyOn(User, "createQueryBuilder").mockReturnValueOnce({
+          where: jest.fn().mockReturnThis(),
+          leftJoinAndSelect: jest.fn().mockReturnThis(),
+          getOne: jest.fn().mockResolvedValueOnce({
+            ...savedUser,
+            save: jest.fn().mockResolvedValueOnce({ ...savedUser }),
+          }),
+        } as any);
+
+        jest.spyOn(jwt, "verify").mockImplementationOnce(
+          () =>
+            ({
+              id,
+              email,
+            } as JwtPayload)
+        );
+        jest.spyOn(jwt, "sign").mockImplementationOnce(() => "signed");
+
+        jest.spyOn(bcrypt, "compare").mockImplementationOnce(() => true);
+
+        const res = await userService.signin(dto);
+
+        expect(res.statusCode).toBe(200);
+        expect(res.message).toContain("success");
+        expect(res.data).toBeDefined();
       });
     });
   });
